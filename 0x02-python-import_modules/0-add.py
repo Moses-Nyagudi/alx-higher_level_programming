44b7e02ea8bf4b8064ae87c9338d06ab4416e45f
Moses-Nyagudi <nyagudi.ulridge@strathmore.edu>
2023-11-30 13:12:32 +0300

    corrections

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-from add_0 import add
 if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-
+# Ensure there is a newline at the end of the file
